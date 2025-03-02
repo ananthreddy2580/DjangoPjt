9221b6c31c1c692ce52381bfb6d5b415fadbd033
Ananth <mandliananth@gmail.com>
2025-03-02 23:08:14 +0530

    changes in route

--- home_app/urls.py
+++ home_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import visitor_submission,get_csrf_token
+from django.http import HttpResponse
+from .views import visitor_submission, get_csrf_token
+
+# Simple function to return a response for "/"
+def home(request):
+    return HttpResponse("Welcome to the Visitor API! Use /visitors/ to submit data.")
 
 urlpatterns = [
+    path('', home, name='home'),  # Add this to fix the 404 error
     path('visitors/', visitor_submission, name='visitor_submission'),
     path('get-csrf-token/', get_csrf_token, name='get_csrf_token'),
-
 ]
